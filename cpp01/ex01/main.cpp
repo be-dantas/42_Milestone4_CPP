d2233ecd5dcd686f5101ddca0dd97d4a2fbc5092
be-dantas <226878089+be-dantas@users.noreply.github.com>
2026-02-14 17:22:41 -0300

    cpp01 ex01

---
+++ cpp01/ex01/main.cpp
@@ -0,0 +1,15 @@
+#include "Zombie.hpp"
+
+int main(void)
+{
+    int N = 5;
+    Zombie* horde;
+
+    horde = zombieHorde(N, "Bias");
+
+    for (int i = 0; i < N; i++)
+        horde[i].announce();
+
+    delete[] horde;
+    return (0);
+}
