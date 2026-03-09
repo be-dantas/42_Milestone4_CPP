725dba410817d20a373d60c1d82be6a5f1fcfba0
Beatriz Dantas <226878089+be-dantas@users.noreply.github.com>
2026-03-09 10:00:20 -0300

    Clean up comments in main.cpp
    Removed commented-out exception handling example for clarity.

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -12,20 +12,6 @@
 
 #include "Bureaucrat.hpp"
 
-/* Bureaucrat("Ana", 0)
-	   ↓
-grade < 1
-	   ↓
-throw GradeTooHighException()
-	   ↓
-programa procura catch
-	   ↓
-catch(std::exception&)
-	   ↓
-e.what()
-	   ↓
-"Grade too high" */
-
 int main()
 {
 	std::cout << "----- Teste 1: criação válida -----" << std::endl;
